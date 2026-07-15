c1666c3ad70f66c9d7d884d1b5fb78e9496b481f
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-15 11:47:51 +0000

    product add

--- Level 2/Cart/script.js
+++ Level 2/Cart/script.js
@@ -6,6 +6,38 @@ var product = [
 ];
 var popular = [];
 
+var popular = "";
+products.forEach(function(product){
+    clutter += ` <div class="product w-fit rounded-xl p-2 bg-white">
+                <div class="image w-[14rem] h-[13rem] bg-zinc-200 rounded-xl"></div>
+                <div class="data w-full px-2 py-5">
+                    <h1 class="font-semibold text-xl leading-none tracking-tight">EJ 100 Ox Chair.</h1>
+                    <div class="flex justify-between w-full items-center mt-2">
+                        <div class="w-1/2">
+                            <h3 class="font-semibold opacity-20">Hangup data.</h3>
+                            <h4 class="font-semibold mt-2">$3,456</h4>
+                        </div>
+                        <button class="w-10 h-10 rounded-full shader text-yellow-400"><i
+                                class="ri-add-line"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="product w-fit rounded-xl p-2 bg-white">
+                <div class="image w-[14rem] h-[13rem] bg-zinc-200 rounded-xl"></div>
+                <div class="data w-full px-2 py-5">
+                    <h1 class="font-semibold text-xl leading-none tracking-tight">EJ 100 Ox Chair.</h1>
+                    <div class="flex justify-between w-full items-center mt-2">
+                        <div class="w-1/2">
+                            <h3 class="font-semibold opacity-20">Hangup data.</h3>
+                            <h4 class="font-semibold mt-2">$3,456</h4>
+                        </div>
+                        <button class="w-10 h-10 rounded-full bg-[#4A4563] text-yellow-400"><i
+                                class="ri-add-line"></i></button>
+                    </div>
+                </div>
+            </div>`;
+})
+
 
 // show real popular products
 // on click of product add button abd it to the cart
