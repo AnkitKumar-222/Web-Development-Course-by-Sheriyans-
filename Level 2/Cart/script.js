91d4902926ec93a84b6eae2820f9282143bd00f4
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-15 10:47:03 +0000

    level 2

---
+++ Level 2/Cart/script.js
@@ -0,0 +1,9 @@
+// show real projects
+var product = [
+    {name : "", headline : "", price: "", image: ""}
+];
+var popular = [];
+
+
+// show real popular products
+// on click of product add button abd it to the cart
