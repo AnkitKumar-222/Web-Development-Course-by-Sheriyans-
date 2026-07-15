e0ef8d9cacf50acd9af264168715a6b6029dfb7c
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-15 11:16:07 +0000

    real projects

--- Level 2/Cart/script.js
+++ Level 2/Cart/script.js
@@ -1,6 +1,8 @@
 // show real projects
 var product = [
-    {name : "", headline : "", price: "", image: ""}
+    {name : "White Chair", headline : "Soft like cloud", price: "15000", image: "https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNoYWlyfGVufDB8fDB8fHww"}
+    {name : "Blue chair", headline: "Ladki ka stool", price: "10000", image: "https://images.unsplash.com/photo-1503602642458-232111445657?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"}
+    {name : "Traditional Chair", headline: "Look very old", price: "16500", image: "https://images.unsplash.com/photo-1506439773649-6e0eb8cfb237?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"}
 ];
 var popular = [];
 
