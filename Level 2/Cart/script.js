2e62fa7490a376baa5641f3c24ba4c5f11594b7c
Ankit kumar <naudiyalankit22@outlook.com>
2026-08-21 08:33:15 +0000

    adding popular

--- Level 2/Cart/script.js
+++ Level 2/Cart/script.js
@@ -1,6 +1,6 @@
 // show real products
 var products = [
-    {name: "White Chair", headline: "Soft like cloud.", price: "15,000", image:"https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNoYWlyfGVufDB8fDB8fHww"},
+    {name: "White Chair", headline: "Soft like cloud.", price: "15,000", image:"https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNoYWlyfGVufDB8fDB8fHww"},
     {name: "Blue Chair", headline: "Lakdi ka stool", price: "10,000", image:"https://images.unsplash.com/photo-1503602642458-232111445657?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
     {name: "Traditional Chair", headline: "Look very old", price: "16,500", image:"https://images.unsplash.com/photo-1506439773649-6e0eb8cfb237?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"}
 ];
@@ -16,44 +16,44 @@ function addProduct(){
 products.forEach(function(product){
     clutter += `<div class="product w-fit rounded-xl p-2 bg-white">
                 <div class="image w-[14rem] h-[13rem] bg-zinc-200 rounded-xl overflow-hidden ">
-                    <img class="w-full h-full object-cover "  scr="${product.image}"/>
-                </div>
-                <div class="data w-full px-2 py-5">
+                    <img class="w-full h-full object-cover "  scr="${product.image}" />
+                    </div>
+                    <div class="data w-full px-2 py-5">
                     <h1 class="font-semibold text-xl leading-none tracking-tight">${product.name}</h1>
                     <div class="flex justify-between w-full items-center mt-2">
                         <div class="w-1/2">
-                            <h3 class="font-semibold opacity-20">${product.head}</h3>
+                            <h3 class="font-semibold opacity-20">${product.headline}</h3>
                             <h4 class="font-semibold mt-2">&#8377;${product.price}</h4>
-                        </div>
-                        <button class="w-10 h-10 rounded-full shader text-yellow-400"><i
+                            </div>
+                                <button class="w-10 h-10 rounded-full shader text-yellow-400"><i
                                 class="ri-add-line"></i></button>
                     </div>
-                </div>
-    </div> ';
+                    </div>
+    </div> `;
 
             })
 
             document.querySelector(".products").innerHTML = clutter;
 }
 
+
 function addPopularProdcuts(){
-    var clutter = "";
-    popular.forEach(function(product){
-        var clutter += ` <div class="popular bg-white p-2 rounded-2xl flex items-start gap-3 w-[60%] flex-shrink-0">
+     var clutter = "";
+        popular.forEach(function(product){
+         clutter += ` <div class="popular bg-white p-2 rounded-2xl flex items-start gap-3 w-[60%] flex-shrink-0">
                     <div class="w-20 h-20 bg-red-500 flex-shrink-0 rounded-2xl border-4 border-white overflow-hidden">
-                        <img class="w-full h-full object-cover"
-                            src="https://images.unsplash.com/photo-1571575173700-afb9492e6a50?q=80&w=3412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            alt="">
+                        <img class="w-full h-full object-cover"  src="https://images.unsplash.com/photo-1617582907226-c49e2d8200d9?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGNoYWlyfGVufDB8fDB8fHww"  alt="">
                     </div>
                     <div class="data py-2 w-full">
-                        <h1 class="leading-none font-semibold">Eyes Lounge</h1>
-                        <h4 class="leading-none mt-2 text-sm font-semibold opacity-20">Bisco Do Lobo</h4>
-                        <h4 class="mt-3 font-semibold text-zinc-500">$12,000</h4>
+                        <h1 class="leading-none font-semibold">${product.name}</h1>
+                        <h4 class="leading-none mt-2 text-sm font-semibold opacity-20">${product.headline}</h4>
+                        <h4 class="mt-3 font-semibold text-zinc-500">&#8377; ${product.price}</h4>
                     </div>
                 </div>`;
-    });
+        });
 }
 
+addPopularProdcuts();
 addProduct();
 
 // show real popular products
