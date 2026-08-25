88ca967aae83f114037da57453d23c66beb7269b
Ankit kumar <naudiyalankit22@outlook.com>
2026-08-25 12:17:21 +0000

    add song

--- Level 2/Music Player/script.js
+++ Level 2/Music Player/script.js
@@ -1,6 +1,6 @@
 var arr = [
     { songName: "Jale 2", url: "./songs/Jale 2.mp3", img: "./images/jale.jpg" },
-    { songName: "Pehle Bhi main", url: "./songs/Pehle Bhi Main.mp3", img: "./images/animal.jpg" },
+    { songName: "Pehle Bhi main", url: "./songs/Pehle Bhi Main.mp3", img: "https://www.jiosaavn.com/album/pehle-bhi-main-remix/nQTsobk,t8k_" },
     { songName: "Ram siya ram", url: "./songs/Ram Siya Ram.mp3", img: "./images/ram.jpg" },
     { songName: "Arjan Valley", url: "./songs/Arjan Vailly Ne.mp3", img: "./images/animal.jpg" }
 ]
